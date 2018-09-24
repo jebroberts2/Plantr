d046bb9d11e175bc988fc8b1f3802b39a572a9fb
Kasim <Kasimalam@gmail.com>
2018-09-24 17:31:18 -0400

    fsdf

--- models.js
+++ models.js
@@ -2,4 +2,5 @@ const Sequalize = require("sequelize");
 const db = new Sequalize("postgres://localhost:5432/plantr");
 
 module.exports = db;
-'pickle rick!'
+("pickle rick!");
+("jdskhfskljhdfkljashdklfjhasdklfjalkjsfhkljasdhfjklahsfkljahdskl jfahsdkljf askl");
